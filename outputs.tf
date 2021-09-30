40212d00cdde5c4981cd3b706c589647ffbd7782
cesar-vergara-e <cvergarae@smu.cl>
2021-09-30 15:47:35 -0300

    test

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,6 @@ output "route53-arn" {
   value = aws_iam_policy.route53.arn
 }
 
-output "autoscaling-resources" {
-  value = aws_eks_node_group.eks-node-group.resources
+output "autoscaling-name" {
+  value = aws_eks_node_group.eks-node-group.resources[0].autoscaling_groups.name
 }
