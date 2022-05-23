e791beb62b6709d80c83e824d6c1a48cd5e75b7a
Cesar Vergara <cvergarae@smu.cl>
2022-05-23 17:17:40 -0400

    test bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@ MIME-Version: 1.0
 Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
 
 --==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
 
+#!/bin/bash
 /etc/eks/bootstrap.sh aws-virginia-factory-front-develop \
   --cni-prefix-delegation-enabled \
   --disk-size 100 \
