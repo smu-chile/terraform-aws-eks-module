274537a4351dbfc22b8691ca5583a63a3a9c0388
Cesar Vergara <cvergarae@smu.cl>
2022-05-23 16:03:29 -0400

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,1 @@
-MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
-
---==MYBOUNDARY==
-
-/etc/eks/bootstrap.sh aws-virginia-factory-front-develop \
-  --cni-prefix-delegation-enabled \
-  --disk-size 100 \
-  --use-max-pods false \
-  --kubelet-extra-args '--max-pods=110'
-
---==MYBOUNDARY==--
+/etc/eks/bootstrap.sh aws-virginia-factory-front-develop --cni-prefix-delegation-enabled --disk-size 100 --use-max-pods false   --kubelet-extra-args '--max-pods=110'
