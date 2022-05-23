362074b2d9617afe438a8935079c75904535e3b7
fjleivad <fjleivad@smu.cl>
2022-05-23 09:52:54 -0400

    launch template test

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -9,6 +9,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   node_group_name = "${var.cluster-name}-default-node-group"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.private-subnet-ids
+  vpc_security_group_ids = module.node-sg.security_group_name
   # disk_size       = 100
 
   launch_template {
