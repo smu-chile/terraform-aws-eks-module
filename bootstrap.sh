c129465b6a92a804f1e6ca238a484dd2d73dd2a9
Cesar Vergara <cvergarae@smu.cl>
2022-05-24 09:48:24 -0400

    test bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,14 @@ Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
 
 --==MYBOUNDARY==
 Content-Type: text/x-shellscript; charset="us-ascii"
-
 #!/bin/bash
+set -ex
+cat <<-EOF > /etc/profile.d/bootstrap.sh
 export USE_MAX_PODS=false
 export KUBELET_EXTRA_ARGS="--max-pods=110"
+EOF
+# Source extra environment variables in bootstrap script
+sed -i '/^set -o errexit/a\\nsource /etc/profile.d/bootstrap.sh' /etc/eks/bootstrap.sh
+EOT
 
 --==MYBOUNDARY==--
