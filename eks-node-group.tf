c8998e9ac86a5397d1575d0c129f403dbfb5002c
fjleivad <fjleivad@smu.cl>
2022-06-01 18:27:23 -0400

    cn-prefix-delegation launch template

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -7,7 +7,7 @@ Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
 Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
 set -o xtrace
-/etc/eks/bootstrap.sh ${var.cluster-name} --cni-prefix-delegation true --use-max-pods false --kubelet-extra-args --max-pods=${var.max-pods}
+/etc/eks/bootstrap.sh ${var.cluster-name} --cni-prefix-delegation-enabled --use-max-pods false --kubelet-extra-args --max-pods=${var.max-pods}
 
 --==MYBOUNDARY==--
 USERDATA
