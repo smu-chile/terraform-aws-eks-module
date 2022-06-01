55571d168785ffd1b450ba8b10fd75686f3034be
jveraduran <51175225+jveraduran@users.noreply.github.com>
2022-06-01 11:14:58 -0400

    Fix: Change ApiVersion Cluster Authentication

--- config.tf
+++ config.tf
@@ -34,7 +34,7 @@ users:
 - name: ${var.cluster-name}
   user:
     exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
+      apiVersion: client.authentication.k8s.io/v1beta1
       command: aws
       args:
         - "eks"
