c517f4c21d780bae720a38b4ce9d3cca9587bcdf
fjleivad <fjleivad@smu.cl>
2022-06-03 11:26:25 -0400

    ipv6 config

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -12,7 +12,9 @@ resource "aws_eks_cluster" "eks" {
 
   enabled_cluster_log_types = var.eks-cw-logging
 
-  ip_family = ipv6
+  kubernetes_network_config {
+    ip_family = ipv6
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
